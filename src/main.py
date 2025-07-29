58af0eedb3726b119b575b2bcc501a188d61cd1c
Erfan Salimi <erfansalimi2007lite@gmail.com>
2025-07-30 00:31:10 +0330

    Added setting button

--- src/main.py
+++ src/main.py
@@ -6,6 +6,25 @@ def main(page: ft.Page):
     page.window.height = 600
     #page.window.frameless = True       Add for new design
     page.title = "Quick DNS Changer"
+    def open_main(e):
+        page.clean()
+        settin_button = ft.IconButton(
+            icon=ft.Icons.SETTINGS,
+            on_click=open_setting)
+        page.add(settin_button)
+        page.update
 
+    def open_setting(e):
+        page.clean()
+
+        back_button = ft.IconButton(
+            on_click=open_main,
+            icon=ft.Icons.ARROW_BACK
+        )
+        
+        page.add(back_button, ft.Text("Comming soon ..."))
+        page.update
+    
+    open_main(None)
 
 ft.app(main)
